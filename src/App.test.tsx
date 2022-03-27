f3a3a1095be8e29df343d4d32d2e886abb2c0d0e
Aleks164 <alekseylehamail.ru>
2022-03-27 12:22:57 +0300

    add act in test app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-promise-executor-return */
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { App } from "./App";
+
 import { textP } from "./components/DefultPage/DefultPage";
 import "@testing-library/jest-dom";
 
@@ -9,69 +11,81 @@ const sleep = (x: number) => new Promise((r) => setTimeout(r, x));
 
 describe("DefultPage", () => {
   it("test hr at DOM", async () => {
-    const spyPrompt = jest.spyOn(window, "prompt");
-    spyPrompt.mockReturnValueOnce("Hr").mockReturnValueOnce("");
-    const { container } = render(<App isLokiTest={false} sleepMs={0} />);
-    await sleep(50);
-    const classAllHr = container.querySelector(".hr");
-    expect(classAllHr).toBeInTheDocument();
+    await act(async () => {
+      const spyPrompt = jest.spyOn(window, "prompt");
+      spyPrompt.mockReturnValueOnce("Hr").mockReturnValueOnce("");
+      const { container } = render(<App isLokiTest={false} sleepMs={0} />);
+      await sleep(50);
+      const classAllHr = container.querySelector(".hr");
+      expect(classAllHr).toBeInTheDocument();
+    });
   });
   it("test Paragraph components", async () => {
-    const spyPrompt = jest.spyOn(window, "prompt");
-    spyPrompt.mockReturnValueOnce("Paragraph").mockReturnValueOnce("");
-    render(<App isLokiTest={false} sleepMs={0} />);
-    await sleep(50);
-    expect(screen.getByText(textP)).toBeInTheDocument();
+    await act(async () => {
+      const spyPrompt = jest.spyOn(window, "prompt");
+      spyPrompt.mockReturnValueOnce("Paragraph").mockReturnValueOnce("");
+      render(<App isLokiTest={false} sleepMs={0} />);
+      await sleep(50);
+      expect(screen.getAllByText(textP).length).toBe(3);
+    });
   });
   it("test Title components", async () => {
-    const spyPrompt = jest.spyOn(window, "prompt");
-    spyPrompt.mockReturnValueOnce("Title").mockReturnValueOnce("");
-    render(<App isLokiTest={false} sleepMs={0} />);
-    await sleep(50);
-    expect(screen.getByText("Click on me")).toBeInTheDocument();
+    await act(async () => {
+      const spyPrompt = jest.spyOn(window, "prompt");
+      spyPrompt.mockReturnValueOnce("Title").mockReturnValueOnce("");
+      render(<App isLokiTest={false} sleepMs={0} />);
+      await sleep(50);
+      expect(screen.getByText("Click on me")).toBeInTheDocument();
+    });
   });
   it("test DefultPage components", async () => {
-    const spyPrompt = jest.spyOn(window, "prompt");
-    spyPrompt.mockReturnValueOnce("");
-    const { container, getByText } = render(
-      <App isLokiTest={false} sleepMs={0} />
-    );
-    await sleep(50);
-    expect(getByText(textP)).toBeInTheDocument();
-    expect(getByText("Click on me")).toBeInTheDocument();
-    expect(getByText("Some text in h1 element")).toBeInTheDocument();
-    expect(getByText('Выберите тег "h"')).toBeInTheDocument();
-    expect(getByText("h1")).toBeInTheDocument();
+    await act(async () => {
+      const spyPrompt = jest.spyOn(window, "prompt");
+      spyPrompt.mockReturnValueOnce("");
+      const { container, getByText } = render(
+        <App isLokiTest={false} sleepMs={0} />
+      );
+      await sleep(50);
+      expect(screen.getAllByText(textP).length).toBe(3);
+      expect(getByText("Click on me")).toBeInTheDocument();
+      expect(getByText("Some text in h1 element")).toBeInTheDocument();
+      expect(getByText('Выберите тег "h"')).toBeInTheDocument();
+      expect(getByText("h1")).toBeInTheDocument();
 
-    const classImage = container.querySelector(".image");
-    expect(classImage).toBeInTheDocument();
-    const classHr = container.querySelector(".hr");
-    expect(classHr).toBeInTheDocument();
+      const classImage = container.querySelector(".image");
+      expect(classImage).toBeInTheDocument();
+      const classHr = container.querySelector(".hr");
+      expect(classHr).toBeInTheDocument();
+    });
   });
   it("test Image components", async () => {
-    const spyPrompt = jest.spyOn(window, "prompt");
-    spyPrompt
-      .mockReturnValueOnce("Image")
-      .mockReturnValueOnce("left")
-      .mockReturnValueOnce("");
-    const { container } = render(<App isLokiTest={false} sleepMs={0} />);
-    await sleep(50);
-    const classImage = container.querySelector(".image");
-    expect(classImage).toBeInTheDocument();
-    if (classImage) {
-      expect((classImage as HTMLElement).style.float).toBe("left");
-    }
+    await act(async () => {
+      const spyPrompt = jest.spyOn(window, "prompt");
+      spyPrompt
+        .mockReturnValueOnce("Image")
+        .mockReturnValueOnce("left")
+        .mockReturnValueOnce("");
+      const { container } = render(<App isLokiTest={false} sleepMs={0} />);
+      await sleep(50);
+      const classImage = container.querySelector(".image");
+      expect(classImage).toBeInTheDocument();
+      if (classImage) {
+        expect((classImage as HTMLElement).style.float).toBe("left");
+      }
+    });
   });
   it("test HSelector components", async () => {
-    const spyPrompt = jest.spyOn(window, "prompt");
-    spyPrompt
-      .mockReturnValueOnce("HSelector")
-      .mockReturnValueOnce("h3")
-      .mockReturnValueOnce("");
-    render(<App isLokiTest={false} sleepMs={0} />);
-    await sleep(50);
-    expect(screen.getByText("Some text in h3 element")).toBeInTheDocument();
-    expect(screen.getByText('Выберите тег "h"')).toBeInTheDocument();
-    expect(screen.getByText("h3")).toBeInTheDocument();
+    await act(async () => {
+      const spyPrompt = jest.spyOn(window, "prompt");
+      spyPrompt
+        .mockReturnValueOnce("HSelector")
+        .mockReturnValueOnce("h3")
+        .mockReturnValueOnce("");
+      render(<App isLokiTest={false} sleepMs={0} />);
+      await sleep(50);
+      expect(screen.getByText("Some text in h3 element")).toBeInTheDocument();
+      expect(screen.getByText('Выберите тег "h"')).toBeInTheDocument();
+      expect(screen.getByText("h3")).toBeInTheDocument();
+    });
   });
 });
